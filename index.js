05e1390bebbd16efea2633bdd5c54e62b00dccbb
Sharbel Sarkis <charbelsako@gmail.com>
2022-06-01 16:59:04 +0000

    Added the base react app, and some boilerplate for later on

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express")
-
+require("dotenv").config()
 const app = express()
 
-// app.use("/auth", authRouter)
-// app.use("/client", clientRouter)
+// routers
+const authRouter = require("./routes/authRouter")
+const clientRouter = require("./routes/clientRouter")
 
+app.use("/auth", authRouter)
+app.use("/client", clientRouter)
+
+// TODO: remove later. just a test
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
 
+console.log(process.env.PORT)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
